fd88bb77ade60e2bc31bc31bee076d516e96a23c
lilei <kevin12138az@163.com>
2018-12-25 20:59:18 +0800

    order dao层

---
+++ cloud-order/src/test/java/com/kevin/order/repository/OrderMasterRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.kevin.order.repository;
+
+import com.kevin.order.CloudOrderApplicationTests;
+import com.kevin.order.dataobject.OrderMaster;
+import com.kevin.order.enums.OrderStatusEnum;
+import com.kevin.order.enums.PayStatusEnum;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+@Component
+public class OrderMasterRepositoryTest extends CloudOrderApplicationTests {
+
+    @Autowired
+    private OrderMasterRepository orderMasterRepository;
+
+    @Test
+    public void testSave() {
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId("1234567");
+        orderMaster.setBuyerName("师兄");
+        orderMaster.setBuyerPhone("1886131241241");
+        orderMaster.setBuyerAddress("慕课网总部");
+        orderMaster.setBuyerOpenid("1101110");
+        orderMaster.setOrderAmount(new BigDecimal(2.5));
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+
+        OrderMaster result = orderMasterRepository.save(orderMaster);
+        Assert.assertTrue(result != null);
+    }
+
+}
