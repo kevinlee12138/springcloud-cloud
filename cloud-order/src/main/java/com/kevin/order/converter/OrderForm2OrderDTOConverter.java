37b5a9479062078b1016cf9bd5dd532f908d3a6e
lilei <kevin12138az@163.com>
2018-12-25 21:55:53 +0800

    order 创建订单接口 完成入库 参数校验

---
+++ cloud-order/src/main/java/com/kevin/order/converter/OrderForm2OrderDTOConverter.java
@@ -0,0 +1,38 @@
+package com.kevin.order.converter;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.kevin.order.dataobject.OrderDetail;
+import com.kevin.order.dto.OrderDTO;
+import com.kevin.order.enums.ResultEnum;
+import com.kevin.order.exception.OrderException;
+import com.kevin.order.form.OrderForm;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class OrderForm2OrderDTOConverter {
+    public static OrderDTO convert(OrderForm orderForm) {
+        Gson gson = new Gson();
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setBuyerName(orderForm.getName());
+        orderDTO.setBuyerPhone(orderForm.getPhone());
+        orderDTO.setBuyerAddress(orderForm.getAddress());
+        orderDTO.setBuyerOpenid(orderForm.getOpenid());
+
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        try {
+            orderDetailList = gson.fromJson(orderForm.getItems(),
+                    new TypeToken<List<OrderDetail>>() {
+                    }.getType());
+        } catch (Exception e) {
+            log.error("【json转换】错误, string={}", orderForm.getItems());
+            throw new OrderException(ResultEnum.PARAM_ERROR);
+        }
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
+    }
+}
